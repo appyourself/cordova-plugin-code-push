287c45875ec2450c5f28c71933931cd28ec6ef08
andrzejidzior <andrzej.idzior@appyourself.net>
2021-10-21 17:41:40 +0200

    ignore files

--- www/fileUtil.ts
+++ www/fileUtil.ts
@@ -126,6 +126,11 @@ class FileUtil {
             ignoreList.push("__MACOSX");
         }
 
+        ignoreList.push("plugins");
+        ignoreList.push("config.js");
+        ignoreList.push("cordova_plugins.js");
+        ignoreList.push("cordova.js");
+
         var fail = (error: FileError) => {
             callback(FileUtil.fileErrorToError(error), null);
         };
