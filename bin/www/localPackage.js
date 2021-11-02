5ef8b6b5a9ab134d64dc406a89eff308b4aaf456
andrzejidzior <andrzej.idzior@appyourself.net>
2021-11-02 18:10:25 +0100

    Revert "disable sync"
    This reverts commit f207dcf69f71ca6b67b72a86d9849ab37391d989.

--- bin/www/localPackage.js
+++ bin/www/localPackage.js
@@ -346,7 +346,6 @@ alert('trzymaj kciuki');
 
 FileUtil.getApplicationDirectory("www", CodePushUtil.getNodeStyleCallbackFor(success, fail));
 
-/*
 
                         LocalPackage.getPackage(LocalPackage.PackageInfoFile, (localPackageCurrentInstalled) => {
                             FileUtil.getDataDirectory(localPackageCurrentInstalled.localPath, true, (deployDirError, oldPackage) => {
@@ -354,10 +353,10 @@ FileUtil.getApplicationDirectory("www", CodePushUtil.getNodeStyleCallbackFor(suc
                                     alert('deployDirError');
                                     alert(JSON.stringify(deployDirError));
                                 }
-                                FileUtil.copyDirectoryEntriesTo(oldPackage, deployDir, [/!*no need to ignore copy anything*!/], (copyError1) => {
+                                FileUtil.copyDirectoryEntriesTo(oldPackage, deployDir, [/*no need to ignore copy anything*/], (copyError1) => {
                                     alert('copyError1');
                                     alert(JSON.stringify(copyError1));
-                                    FileUtil.copyDirectoryEntriesTo(unzipDir, deployDir, [/!*no need to ignore copy anything*!/], (copyError2) => {
+                                    FileUtil.copyDirectoryEntriesTo(unzipDir, deployDir, [/*no need to ignore copy anything*/], (copyError2) => {
 
                                         if (copyError) {
                                             alert('copyError2');
@@ -373,7 +372,6 @@ FileUtil.getApplicationDirectory("www", CodePushUtil.getNodeStyleCallbackFor(suc
                             alert('err');
                             alert(err.message);
                         });
-*/
 
 
 
